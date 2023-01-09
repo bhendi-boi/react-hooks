1018bbe0a62e516460b3912eeee2629c37ed6c7c
bhendi-boi <bhendiisgreen@gmail.com>
2023-01-09 15:26:57 +0530

    useTheme typescript added

---
+++ typescript/useTheme.tsx
@@ -0,0 +1,40 @@
+import { useState, useEffect } from "react";
+
+type useThemeReturnTypes = {
+  theme: string;
+  toggleTheme: () => void;
+};
+
+const useTheme = (): useThemeReturnTypes => {
+  const prevTheme = localStorage.getItem("theme");
+  const [theme, setTheme] = useState(() => {
+    if (prevTheme) {
+      return prevTheme;
+    }
+    return window.matchMedia("(prefers-color-scheme: dark)").matches
+      ? "dark"
+      : "light";
+  });
+
+  useEffect(() => {
+    const root = window.document.documentElement;
+    if (theme === "dark") {
+      root.classList.remove("light");
+      root.classList.add("dark");
+    } else {
+      root.classList.remove("dark");
+      root.classList.add("light");
+    }
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme((prev) => (prev === "light" ? "dark" : "light"));
+  };
+  return {
+    theme,
+    toggleTheme,
+  };
+};
+
+export default useTheme;
