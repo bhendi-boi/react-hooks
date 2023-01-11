79df68ac1e3e7c750da13a3adcff8bb428a161d1
bhendi-boi <bhendiisgreen@gmail.com>
2023-01-11 20:29:45 +0530

    updated useLocalStorage.tsx

--- typescript/useLocalStorageState.tsx
+++ typescript/useLocalStorageState.tsx
@@ -5,22 +5,22 @@ import { useState, useEffect } from "react";
 //              use signIndata for storing data state which is in signIn page
 //        initialValue : initial value which you want to set (same as useState)
 const useLocalStorageState = (key: string, initialValue: any) => {
-	const [value, setValue] = useState(() => {
-		return getSavedValue(key, initialValue);
-	});
+  const [value, setValue] = useState(() => {
+    return getSavedValue(key, initialValue);
+  });
 
-	useEffect(() => {
-		localStorage.setItem(key, JSON.stringify(value));
-	}, [value]);
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [value]);
 
-	return [value, setValue];
+  return [value, setValue];
 };
 
 const getSavedValue = (key: string, initialValue: any) => {
-	const savedValue = JSON.parse(localStorage.getItem(key) || "");
-	if (savedValue) return savedValue;
-	if (initialValue instanceof Function) return initialValue();
-	return initialValue;
+  const savedValue = JSON.parse(localStorage.getItem(key) || "");
+  if (savedValue) return savedValue;
+  if (initialValue instanceof Function) return initialValue();
+  return initialValue;
 };
 
 export default useLocalStorageState;
